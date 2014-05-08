7bf29a69e74a8ab0adf0d741b440e2f428b72771
Bo Simonsen <bo@geekworld.dk>
2014-05-08 18:13:39 +0200

    Initial implementation of landing page

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -5,18 +5,8 @@ angular.module('natmusSamlingerApp')
     $scope.menu = [{
       'title': 'Home',
       'link': '/'
-    }, {
-      'title': 'Settings',
-      'link': '/settings'
     }];
-    
-    $scope.logout = function() {
-      Auth.logout()
-      .then(function() {
-        $location.path('/login');
-      });
-    };
-    
+
     $scope.isActive = function(route) {
       return route === $location.path();
     };
--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express'),
     path = require('path'),
     fs = require('fs'),
-    mongoose = require('mongoose');
+    cip = require('./lib/cip-methods.js');
 
 /**
  * Main application file
@@ -13,7 +13,6 @@ var express = require('express'),
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var config = require('./lib/config/config');
-var db = mongoose.connect(config.mongo.uri, config.mongo.options);
 
 // Bootstrap models
 var modelsPath = path.join(__dirname, 'lib/models');
@@ -23,12 +22,6 @@ fs.readdirSync(modelsPath).forEach(function (file) {
   }
 });
 
-// Populate empty DB with sample data
-require('./lib/config/dummydata');
-
-// Passport Configuration
-var passport = require('./lib/config/passport');
-
 // Setup Express
 var app = express();
 require('./lib/config/express')(app);
@@ -36,7 +29,7 @@ require('./lib/routes')(app);
 
 // Start server
 app.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
+    console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
 });
 
 // Expose app
