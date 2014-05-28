789bd97834553098836849d80eb4e70c65480dde
Bo Simonsen <bo@geekworld.dk>
2014-05-28 12:26:58 +0200

    Setting up elasticsearch

--- server.js
+++ server.js
@@ -3,24 +3,23 @@
 var express = require('express'),
     path = require('path'),
     fs = require('fs'),
-    cip = require('./lib/cip-methods.js');
+    cip = require('./lib/cip-methods.js'),
+    elasticsearch = require('elasticsearch');;
 
-/**
- * Main application file
- */
-
-// Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var config = require('./lib/config/config');
 
-// Setup Express
 var app = express();
 require('./lib/config/express')(app);
 require('./lib/routes')(app);
 
+var es_client = new elasticsearch.Client();
+app.set('es_client', es_client);
+
 // Start server
 app.listen(config.port, config.ip, function () {
+
     console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
 });
 
