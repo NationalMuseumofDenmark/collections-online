03fd63c8aee6229ca7fa89fca0a145d8c598e10b
Bo Simonsen <bo@geekworld.dk>
2014-05-30 13:44:02 +0200

    Initial categories implementation

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express'),
     path = require('path'),
     fs = require('fs'),
     cip = require('./lib/cip-methods.js'),
+    cip_categories = require('./lib/cip-categories.js'),
     elasticsearch = require('elasticsearch');;
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -16,6 +17,16 @@ require('./lib/routes')(app);
 
 var es_client = new elasticsearch.Client();
 app.set('es_client', es_client);
+
+cip.init_session(function(nm) {
+    cip.get_catalogs(nm, function(catalogs) {
+        cip.get_category_tree(nm, catalogs[1], function(response) {
+            var categories = new cip_categories.Categories(response, function(tree) {
+                console.log(tree); 
+            });
+        });
+    });
+});
 
 // Start server
 app.listen(config.port, config.ip, function () {
