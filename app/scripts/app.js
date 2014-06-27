ee1da008b0d296d0ea5df23b23c4302df8c18456
Bo Simonsen <bo@geekworld.dk>
2014-06-27 14:21:08 +0200

    Restructuring and added categories

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,20 +10,47 @@ var app = angular.module('natmusSamlingerApp', [
 
 app.controller('searchController', function($scope, $http, ngProgress) {
     $scope.results = [];
-    $scope.offset = 0;
+    $scope.catalogs = [];
+    $scope.categories = [];
+
     $scope.loading = false;
     $scope.updated = false;
+
+    $scope.catalog = '';
     $scope.q = '';
+
     $scope.ngProgress = ngProgress;
+
+    $scope.offset = 0;
 
     $scope.$watch('loading', function(value) {
         // Add spinner or something
     });
 
+    $scope.getBaseUrl = function() {
+        var base_url = 'http://' + window.location.host + window.location.pathname;
+
+        if(base_url.slice(-1) != '/') {
+            base_url = base_url + '/';
+        }
+
+        return base_url;
+    };
+
     $scope.doSearch = function() {
         $scope.offset = 0;
         $scope.results = [];
         $scope.nextPage();
+    };
+
+    $scope.setCatalog = function(alias) {
+        $scope.catalog = alias;
+
+        if($scope.catalog != '') {
+            $scope.loadCategories();
+        }
+
+        $scope.doSearch();
     };
 
     $scope.isLoading = function() {
@@ -33,11 +60,11 @@ app.controller('searchController', function($scope, $http, ngProgress) {
     $scope.nextPage = function() {
         $scope.loading = true;
 
-        var base_url = 'http://' + window.location.host + window.location.pathname;
-        if(base_url.slice(-1) != '/') {
-            base_url = base_url + '/';
+        var url = $scope.getBaseUrl();
+        if($scope.catalog != '') {
+            url = url + $scope.catalog + '/';
         }
-        var url = base_url + "search.json?offset=" + $scope.offset;
+        url = url + "search.json?offset=" + $scope.offset;
         url = url + '&q=' + $scope.q;
 
         $http.get(url).success(function(data) {
@@ -50,6 +77,22 @@ app.controller('searchController', function($scope, $http, ngProgress) {
             $scope.loading = false;
         }.bind($scope));
     };
+
+    $scope.loadCatalogs = function() {
+        var url = $scope.getBaseUrl() + 'catalogs.json';
+        $http.get(url).success(function(data) {
+            $scope.catalogs = data;
+        });
+    };
+
+    $scope.loadCategories = function() {
+        var url = $scope.getBaseUrl() + $scope.catalog + '/categories.json';
+        $http.get(url).success(function(data) {
+            $scope.categories = [data];
+        });
+    };
+
+    $scope.loadCatalogs();
 });
 
 
