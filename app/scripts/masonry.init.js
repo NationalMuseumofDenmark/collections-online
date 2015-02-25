c3c55b1135c2489c6d1f3abaf80c99589b89a02c
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-02-25 18:52:19 +0100

    Reverting to a non-jQuery interface to mansonry.

--- app/scripts/masonry.init.js
+++ app/scripts/masonry.init.js
@@ -3,47 +3,49 @@
 // Initiate masonry & infinite scroll on relevant pages
 $(function() {
 
-    var $container = $('#masonry-container');
+	var $container = $('#masonry-container');
 
-    $container.imagesLoaded(function(){
-      $container.masonry({
-        itemSelector: '.box',
-      });
-    });
+	$container.imagesLoaded(function(){
+		var masonry = new Masonry( $container.get(0), {
+			itemSelector: '.box'
+		});
+		$container.data('masonry', masonry);
+	});
 
-    $container.infinitescroll({
-      navSelector  : '#page-nav',    // selector for the paged navigation
-      nextSelector : '#page-nav a',  // selector for the NEXT link (to page 2)
-      itemSelector : '.box',     // selector for all items you'll retrieve
-      loading: {
-          finishedMsg: 'Ikke flere resultater...',
-          img: '/images/loading.gif',
-          msgText: 'Henter flere resultater...',
-          speed: 0,
-        },
-        animate:false
-      },
-      // trigger Masonry as a callback
-      function( newElements ) {
-        // hide new items while they are loading
-        var $newElems = $( newElements ).css({ opacity: 0 });
-        // ensure that images load before adding to masonry layout
-        $newElems.imagesLoaded(function(){
-          // show elems now they're ready
-          $newElems.animate({ opacity: 1 });
-          $container.masonry( 'appended', $newElems, true );
-          $('#more').show();
-        });
-      }
-    );
+	$container.infinitescroll({
+		navSelector  : '#page-nav',    // selector for the paged navigation
+		nextSelector : '#page-nav a',  // selector for the NEXT link (to page 2)
+		itemSelector : '.box',     // selector for all items you'll retrieve
+		loading: {
+				finishedMsg: 'Ikke flere resultater...',
+				img: '/images/loading.gif',
+				msgText: 'Henter flere resultater...',
+				speed: 0,
+			},
+			animate:false
+		},
+		// trigger Masonry as a callback
+		function( newElements ) {
+			// hide new items while they are loading
+			var $newElems = $( newElements ).css({ opacity: 0 });
+			// ensure that images load before adding to masonry layout
+			$newElems.imagesLoaded(function(){
+				// show elems now they're ready
+				$newElems.animate({ opacity: 1 });
+				var masonry = $container.data('masonry');
+				masonry.appended( $newElems, true );
+				$('#more').show();
+			});
+		}
+	);
 
-    $(window).unbind('.infscr');
+	$(window).unbind('.infscr');
 
-    $("#more").click(function(){
-        $('#more').hide();
-        var $container = $('#masonry-container');
-        $container.infinitescroll('retrieve');
-        return false;
-    });
+	$("#more").click(function(){
+			$('#more').hide();
+			var $container = $('#masonry-container');
+			$container.infinitescroll('retrieve');
+			return false;
+	});
 
 });
