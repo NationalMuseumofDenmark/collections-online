707fb6203d82adf4ac6091398287de5a92175139
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-04-28 01:15:45 +0200

    Adding a test to exercise the cip.binary_to_object

---
+++ misc/tests/cip-binary-to-object.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var cip = require('../../lib/cip-methods.js');
+var fs = require('fs');
+var path = require('path');
+var assert = require('assert');
+
+var data_filename = path.resolve(__dirname, 'related.base64');
+
+fs.readFile(data_filename, function (err, data) {
+	if (err) {
+		throw err;
+	}
+	
+	var data_string = data.toString('utf8');
+	var object = cip.binary_to_object( data_string );
+
+	// Assetions.
+	assert(object !== undefined,
+		'The object returned from binary_to_object was expected to be defined.');
+	assert(object.length === 23,
+		'Expected the datafile to contain 23 referenced assets.');
+
+	console.log( JSON.stringify(object, null, 4) );
+});
