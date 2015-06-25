ee74faaaefee25342e40cd1a4141503f516a28c1
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-06-25 02:47:33 +0200

    Adding a check on the index existance.

--- indexing/initialize/elastic-search-index.js
+++ indexing/initialize/elastic-search-index.js
@@ -8,21 +8,31 @@
 function elasticSearchIndex(state) {
 	console.log('Initializing the Elastic Search index');
 
-	return state.es.indices.create({
-		index: process.env.ES_INDEX || 'assets'
-	}).then(function() {
-		console.log('Index created.');
-		return state;
-	}, function(err) {
-		// TODO: Add a recursive check for this message.
-		if(err.message === 'No Living connections') {
-			throw new Error( 'Is the Elasticsearch server running?' );
-		} else if(err.message === 'IndexAlreadyExistsException[[assets] already exists]') {
+	var index = process.env.ES_INDEX || 'assets';
+
+	return state.es.indices.exists({
+		index: index
+	}).then(function(exists) {
+		if(exists) {
 			console.log('Index was already created.');
 			return state;
+		} else {
+			return state.es.indices.create({
+				index: index
+			}).then(function() {
+				console.log('Index created.');
+				return state;
+			}, function(err) {
+				// TODO: Add a recursive check for this message.
+				if(err.message === 'No Living connections') {
+					throw new Error( 'Is the Elasticsearch server running?' );
+				} else {
+					throw err;
+				}
+			});
 		}
-		console.log('Failed to initialize the ElasticSearch index:', err);
 	});
+
 }
 
 module.exports = elasticSearchIndex;
