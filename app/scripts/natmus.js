b0a354f039f3d49b135ada5275866446f423be67
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2015-09-01 12:35:13 +0200

    Fixing a lot of .jshint issues - primarily indention.

--- app/scripts/natmus.js
+++ app/scripts/natmus.js
@@ -9,108 +9,107 @@ $('.asset .text-row').insertAfter('.asset .image-row');
 
 // Open / close main menu nav
 $(function() {
-    $('#heading-categories-menu a, .gray-overlay').click(function(e) {
-      // Show/hide scroll bars with delay due to css animations
-      if ($('body').hasClass('categories-menu-open')) {
-        $('.nicescroll-rails').hide();
-      } else {
-        $('.nicescroll-rails').delay(700).fadeIn(300);
-      }
+    $('#heading-categories-menu a, .gray-overlay').click(function() {
+        // Show/hide scroll bars with delay due to css animations
+        if ($('body').hasClass('categories-menu-open')) {
+            $('.nicescroll-rails').hide();
+        } else {
+            $('.nicescroll-rails').delay(700).fadeIn(300);
+        }
 
-      $('body').toggleClass('categories-menu-open');
+        $('body').toggleClass('categories-menu-open');
 
-      return false;
+        return false;
     });
 });
 
 
 // Typehead autosuggest for search field
 $(function() {
-  var searchSuggest = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    remote: '/suggest.json?text=%QUERY'
-  });
+    var searchSuggest = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: '/suggest.json?text=%QUERY'
+    });
 
-  searchSuggest.initialize();
+    searchSuggest.initialize();
 
-  $('#search-input').typeahead({
-    hint: true,
-    highlight: true,
-    minLength: 2
-  }, {
-    name: 'dropdown-menu',
-    displayKey: function(data) {
-      return data.text;
-    },
-    source: searchSuggest.ttAdapter(),
-    templates: {
-      suggestion: function (data) { return data.text; }
-    }
-  });
+    $('#search-input').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 2
+    }, {
+        name: 'dropdown-menu',
+        displayKey: function(data) {
+            return data.text;
+        },
+        source: searchSuggest.ttAdapter(),
+        templates: {
+            suggestion: function (data) { return data.text; }
+        }
+    });
 });
 
 // Retrieve the main menu and its childern
 $(function() {
-  $.ajax({url: '/catalogs', // Append path to mark active
-  })
-  .done(function(data) {
-    var $menu = $('.categories-menu .dropdown-menu-right');
-    $menu.html(data);
-    
-    $menu.niceScroll({
-      cursorcolor:'#555',
-      background: '',
-      cursorwidth: '7px',
-      cursorborder: 'none',
-      autohidemode: false,
-      horizrailenabled: false,
-      zindex: 999999
-    });
+    $.ajax({url: '/catalogs', // Append path to mark active
+    })
+    .done(function(data) {
+        var $menu = $('.categories-menu .dropdown-menu-right');
+        $menu.html(data);
 
-    // Expand menus
-    $('.categories-menu ul a.expand-menu').click(function(e) {
-      e.preventDefault();
-      console.log(e);
-      var $toggleButton = $(this);
-      $toggleButton.next('ul').slideToggle(300, function() {
-        // Update the scrollbar
-        $menu.getNiceScroll().resize();
-        // Update the icon on the toggle button
-        var expanded = $(this).is(':visible');
-        $toggleButton.closest('li').toggleClass('expanded', expanded);
-      });
+        $menu.niceScroll({
+            cursorcolor:'#555',
+            background: '',
+            cursorwidth: '7px',
+            cursorborder: 'none',
+            autohidemode: false,
+            horizrailenabled: false,
+            zindex: 999999
+        });
 
+        // Expand menus
+        $('.categories-menu ul a.expand-menu').click(function(e) {
+            e.preventDefault();
+            console.log(e);
+            var $toggleButton = $(this);
+            $toggleButton.next('ul').slideToggle(300, function() {
+                // Update the scrollbar
+                $menu.getNiceScroll().resize();
+                // Update the icon on the toggle button
+                var expanded = $(this).is(':visible');
+                $toggleButton.closest('li').toggleClass('expanded', expanded);
+            });
+        });
+    })
+    .fail(function() {
+        $('.categories-menu .dropdown-menu-right').html('<li><a href="' + window.location.pathname + '" class="col-xs-12">Uups, der skete en fejl. Prøv at genindlæse siden...</a></li>');
     });
-  })
-  .fail(function(data) {
-    $('.categories-menu .dropdown-menu-right').html('<li><a href="' + window.location.pathname + '" class="col-xs-12">Uups, der skete en fejl. Prøv at genindlæse siden...</a></li>');
-  });
 });
 
 
 // Toogle asset images - zome in and out
 $(function() {
-  $('.asset-image').click(function(e) {
-    if ($(this).parent('div').hasClass('col-md-6')) {
-      $(this).parent('div').removeClass('col-md-6');
-      $(this).parent('div').addClass('col-md-12');
-      // Also for the div below
-      $(this).parent('div').next('div').removeClass('col-md-6');
-      $(this).parent('div').next('div').addClass('col-md-12');
-    } else {
-      $(this).parent('div').removeClass('col-md-12');
-      $(this).parent('div').addClass('col-md-6');
-      // Also for the div below
-      $(this).parent('div').next('div').removeClass('col-md-12');
-      $(this).parent('div').next('div').addClass('col-md-6');
-    }
-  });
+    $('.asset-image').click(function() {
+        if ($(this).parent('div').hasClass('col-md-6')) {
+            $(this).parent('div').removeClass('col-md-6');
+            $(this).parent('div').addClass('col-md-12');
+            // Also for the div below
+            $(this).parent('div').next('div').removeClass('col-md-6');
+            $(this).parent('div').next('div').addClass('col-md-12');
+        } else {
+            $(this).parent('div').removeClass('col-md-12');
+            $(this).parent('div').addClass('col-md-6');
+            // Also for the div below
+            $(this).parent('div').next('div').removeClass('col-md-12');
+            $(this).parent('div').next('div').addClass('col-md-6');
+        }
+    });
 });
 
 // Scroll to top button
 $(function() {
-  $("#toTop").scrollToTop(400);
+    $('#toTop').scrollToTop(400);
 });
 
 
