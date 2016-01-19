741c64f7a7f5142cc9e1a3401f69969585214f30
Viktor Shamal Andersen <Sphinxominator@gmail.com>
2016-01-19 16:34:45 +0100

    map now resizes to fit asset image height + added overlay button to hide map

--- app/scripts/geotagging.js
+++ app/scripts/geotagging.js
@@ -1,9 +1,30 @@
 (function($) {
+  var resizeMap = function(){
+    var assetImgHeight = $('.primary-asset img').height()
+    $('#geotagging-map').height(assetImgHeight);
+
+    if($('#geotagging-map').is(':visible')){
+      google.maps.event.trigger(map, 'resize');
+    }
+  }
+
+  // resize google map to match asset image on load and on window resize
+  $( window ).resize(function() {
+    resizeMap();
+  }).trigger('resize');
+
   $('.call-to-action .btn').click(function() {
-    $('#geotagging-map').slideToggle('slow');
+    $('.map-container').slideDown('slow');
+    $(this).hide();
+    google.maps.event.trigger(map, 'resize');
   });
 
-  $('.save-coordinates .btn').click(function() {
+  $('.map-buttons .hide-map').click(function() {
+    $('.map-container').slideUp('slow');
+    $('.call-to-action .btn').show();
+  });
+
+  $('.map-buttons .save-coordinates').click(function() {
     if(map.getStreetView().getVisible()){
       console.log(map.getStreetView().getPosition().lat());
       console.log(map.getStreetView().getPosition().lng());
@@ -13,7 +34,6 @@
       console.log(marker.getPosition().lat());
       console.log(marker.getPosition().lng());
     }
-
   });
 })(jQuery);
 
@@ -28,8 +48,12 @@ function initMap() {
 
   // Place a draggable marker on the map
   marker = new google.maps.Marker({
-      position: myLatlng,
-      map: map,
-      draggable:true
+    position: myLatlng,
+    map: map,
+    draggable:true
+  });
+
+  google.maps.event.addListener(map, 'click', function(event) {
+    marker.setPosition(event.latLng);
   });
 }
