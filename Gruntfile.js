ea733d3fd054079292d7eb5652abbba04dd1ff99
Andreas Larsen <andreas@larsenwork.com>
2016-02-08 14:11:25 +0100

    newer elasticsearch, bower as dependency, no node-inspector

--- Gruntfile.js
+++ Gruntfile.js
@@ -127,15 +127,6 @@ module.exports = function (grunt) {
             }
         },
 
-        // Debugging with node inspector
-        'node-inspector': {
-            custom: {
-                options: {
-                    'web-host': 'localhost'
-                }
-            }
-        },
-
         // Use nodemon to run server in debug mode with an initial breakpoint
         nodemon: {
             debug: {
@@ -407,8 +398,7 @@ module.exports = function (grunt) {
             ],
             debug: {
                 tasks: [
-                    'nodemon',
-                    'node-inspector'
+                    'nodemon'
                 ],
                 options: {
                     logConcurrentOutput: true
