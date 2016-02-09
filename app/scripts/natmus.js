001fdbf684871bf73adff5afc8516fff7ca4348b
Andreas Larsen <andreas@larsenwork.com>
2016-02-09 20:15:55 +0100

    natmus js cleanup

--- app/scripts/natmus.js
+++ app/scripts/natmus.js
@@ -1,69 +1,40 @@
 'use strict';
-/*global $ */
-////////////////////////////////////////////////////
-// JS file containing logic used a cross the site //
-////////////////////////////////////////////////////
-
-// Typehead autosuggest for search field
-// Currently not functioning
-/*$(function() {
-    var searchSuggest = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        remote: '/suggest.json?text=%QUERY'
-    });
-
-    searchSuggest.initialize();
-
-    $('#search-input').typeahead({
-        hint: true,
-        highlight: true,
-        minLength: 2
-    }, {
-        name: 'dropdown-menu',
-        displayKey: function(data) {
-            return data.text;
-        },
-        source: searchSuggest.ttAdapter(),
-        templates: {
-            suggestion: function (data) { return data.text; }
-        }
-    });
-});*/
 
 // Retrieve the main menu and its childern
 $(function() {
-    var $menu = $('.categories-menu .dropdown-menu-right');
+  var $menu = $('.categories-menu .dropdown-menu-right');
 
-    // Open / close main menu nav
-    $('#heading-categories-menu, .gray-overlay').click(function() {
-        $('body').toggleClass('categories-menu-open');
-        return false;
-    });
+  // Open / close main menu nav
+  $('#heading-categories-menu, .gray-overlay').click(function() {
+    $('body').toggleClass('categories-menu-open');
+    return false;
+  });
 
-    $.ajax({url: '/catalogs', // Append path to mark active
+  $.ajax({
+      url: '/catalogs', // Append path to mark active
     })
     .done(function(data) {
-        $menu.html(data);
+      $menu.html(data);
 
-        // Expand menus
-        $('a.expand-menu').click(function(e) {
-            e.preventDefault();
+      // Expand menus
+      $('a.expand-menu').click(function(e) {
+        e.preventDefault();
         var $toggleButton = $(e.currentTarget);
         var $use = $toggleButton.children().children();
         var $icon = $use.attr('xlink:href');
+        // Show minus icon if plus and vice versa
         if($icon === '#icon-plus'){
           $use.attr('xlink:href', '#icon-minus');
         } else {
           $use.attr('xlink:href', '#icon-plus');
         }
         $toggleButton.parent().next('ul').slideToggle(100, function() {
-                // Update the icon on the toggle button
-                var expanded = $(this).is(':visible');
+          // Update the icon on the toggle button
+          var expanded = $(this).is(':visible');
           $toggleButton.closest('li').toggleClass('expanded',
             expanded);
-            });
         });
+      });
     })
     .fail(function() {
       $('.categories-menu .dropdown-menu-right').html('<li><a href="' +
@@ -75,25 +46,25 @@ $(function() {
 
 // Toogle asset images - zome in and out
 $(function() {
-    // We only want zooming on asset's primary images.
-    $('.asset .primary-asset').click(function() {
-        if ($(this).parent('div').hasClass('col-md-6')) {
-            $(this).parent('div').removeClass('col-md-6');
-            $(this).parent('div').addClass('col-md-12');
-            // Also for the div below
-            $(this).parent('div').next('div').removeClass('col-md-6');
-            $(this).parent('div').next('div').addClass('col-md-12');
-        } else {
-            $(this).parent('div').removeClass('col-md-12');
-            $(this).parent('div').addClass('col-md-6');
-            // Also for the div below
-            $(this).parent('div').next('div').removeClass('col-md-12');
-            $(this).parent('div').next('div').addClass('col-md-6');
-        }
-    });
+  // We only want zooming on asset's primary images.
+  $('.asset .primary-asset').click(function() {
+    if ($(this).parent('div').hasClass('col-md-6')) {
+      $(this).parent('div').removeClass('col-md-6');
+      $(this).parent('div').addClass('col-md-12');
+      // Also for the div below
+      $(this).parent('div').next('div').removeClass('col-md-6');
+      $(this).parent('div').next('div').addClass('col-md-12');
+    } else {
+      $(this).parent('div').removeClass('col-md-12');
+      $(this).parent('div').addClass('col-md-6');
+      // Also for the div below
+      $(this).parent('div').next('div').removeClass('col-md-12');
+      $(this).parent('div').next('div').addClass('col-md-6');
+    }
+  });
 });
 
 // Scroll to top button
 $(function() {
-    $('#toTop').scrollToTop(400);
+  $('#toTop').scrollToTop(400);
 });
