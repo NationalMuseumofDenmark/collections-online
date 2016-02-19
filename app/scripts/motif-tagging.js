5094d90ae820ac3cef25ab06713445ac70b30c17
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-02-19 13:47:25 +0100

    Moved the geo-tagging and motif-tagging scripts into the asset template

---
+++ app/scripts/motif-tagging.js
@@ -0,0 +1,27 @@
+var $asset = $('.asset');
+var $tags = $('.asset .tags');
+$('.add-tags-btn', $asset).click(function() {
+  $.ajax({
+    dataType: 'json',
+    url: window.location + '/suggested-motif-tags'
+  }).done(function(data) {
+    console.log(data.tags);
+    var arrayLength = data.tags.length;
+    if(arrayLength !== 0) {
+      for (var i = 0; i < arrayLength; i++) {
+        var tag = data.tags[i];
+        // var tagUrl = '/?q=' + encodeURIComponent(tag);
+        $tag = $('<span class="btn btn-primary">' + tag + '</span>');
+        $icon = $('<svg><use xlink:href="#icon-minus" /></svg>');
+        $tag.append($icon);
+        $tags.append($tag);
+        $tag.click(function(e) {
+            $(this).remove();
+        });
+      }
+    }
+  }).fail(function() {
+    console.log('Ajax failed to fetch data');
+  });
+  $('.add-tags-btn').hide();
+});
