79e3ad222433594f8f0729eb8bd7348de8109750
Andreas Larsen <andreas@larsenwork.com>
2016-02-25 10:29:21 +0100

    tags reorder

--- app/scripts/motif-tagging.js
+++ app/scripts/motif-tagging.js
@@ -1,29 +1,31 @@
 'use strict';
 
 var $asset = $('.asset');
-var $tags = $('.asset .tags');
+var $tags = $('.asset .tags.vision');
 $('.add-tags-btn', $asset).click(function() {
   $(this).addClass('loading');
+  $('.vision .no-tags').remove();
   $('.add-tags-btn .text').remove();
   $.ajax({
     dataType: 'json',
     url: window.location + '/suggested-motif-tags'
   }).done(function(data) {
     $('.add-tags-btn').remove();
-    $('.no-tags').hide();
     console.log(data.tags);
     var arrayLength = data.tags.length;
     if(arrayLength !== 0) {
       for (var i = 0; i < arrayLength; i++) {
-        // var tagUrl = '/?q=' + encodeURIComponent(tag);
         var tag = data.tags[i];
-        var $tag = $('<span class="btn btn-primary">' + tag + '</span>');
-        var $icon = $('<svg><use xlink:href="#icon-minus" /></svg>');
-        $tag.append($icon);
+        var tagUrl = '/?q=' + encodeURIComponent(tag);
+        var $tag = $('<a href="' + tagUrl + '" class="tag">' + tag + '</a>');
         $tags.append($tag);
-        $tag.click(function() {
-          $(this).remove();
-        });
+        // var $icon = $('<svg><use xlink:href="#icon-minus" /></svg>');
+        // $tag.append($icon);
+        // var $tag = $('<span class="btn btn-primary">' + tag + '</span>');
+        // var $icon = $('<svg><use xlink:href="#icon-minus" /></svg>');
+        // $tag.click(function() {
+        //   $(this).remove();
+        // });
       }
     }
   }).fail(function() {
