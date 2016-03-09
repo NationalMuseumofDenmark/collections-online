5a11f708ae37aa08a95a67297b816046c894ca96
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-03-09 11:26:15 +0100

    Removing the 'wrong' string

--- indexing/post-processing/clear-index.js
+++ indexing/post-processing/clear-index.js
@@ -9,7 +9,7 @@ var es = require('../../lib/services/elasticsearch');
 module.exports = function(state) {
   if (state.mode === 'clear') {
     return es.indices.delete({
-      index: state.index + 'wrong'
+      index: state.index
     }).then(function() {
       console.log('Index cleared.');
       return state;
