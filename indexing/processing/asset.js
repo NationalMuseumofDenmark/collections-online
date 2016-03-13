862648e4d6c6c90d72fcc0daaf579c1c0f89bd95
Kristoffer Olesen <kristofferdo@gmail.com>
2016-03-13 19:39:44 +0100

    Fix error messaging when err.stack / err.message isn’t defined in indexing

--- indexing/processing/asset.js
+++ indexing/processing/asset.js
@@ -64,7 +64,7 @@ function processAsset(state, metadata, transformations) {
       console.log('Successfully indexed ' + resp._id);
       return resp._id;
     }, function(err) {
-      console.error('An error occured!', err.stack || err.message);
+      console.error('An error occured!', err.stack || err.message || err);
       return new AssetIndexingError(metadata.catalog, metadata.id, err);
     });
 }
