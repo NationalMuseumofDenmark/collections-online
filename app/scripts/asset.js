4b9fa7152a6e9027e2263b7f01acf526dcb396de
Andreas Larsen <andreas@larsenwork.com>
2016-03-16 17:04:42 +0100

    asset image zoom

--- app/scripts/asset.js
+++ app/scripts/asset.js
@@ -2,12 +2,12 @@
 
 (function($, window) {
   var ACTION_ASSET_DOWNLOAD_SHOW = '[data-action="asset-download-show"]';
+  var ACTION_BIG_IMAGE_TOGGLE = '[data-action="asset-image-size-toggle"]';
   var CONTENT_ASSET_DOWNLOAD = '[data-content="asset-download"]';
+  var CONTENT_BIG_IMG_WRAP = '.image-wrapper';
+  var CONTENT_BIG_IMG_CLASS = 'big-image';
   var OVERLAY_ACTIVE_CLASS = 'overlay__container--active';
   var OVERLAY_ANIM_IN_CLASS = 'overlay__container--anim-in';
-
-  // var ACTION_FULLSCREEN_SHOW = '[data-action="fullscreen-show"]';
-  // var CONTENT_FULLSCREEN_WRAP = '.image-wrapper';
 
   var AssetPage = {
     init: function() {
@@ -15,8 +15,10 @@
         .on('click', this.actionAssetDownloadShow.bind(this, true));
       $(CONTENT_ASSET_DOWNLOAD)
         .on('click', this.actionAssetDownloadShow.bind(this, false));
-      // $(ACTION_FULLSCREEN_SHOW)
-      //   .on('click', this.openFullscreen.bind(this));
+      $(CONTENT_BIG_IMG_WRAP)
+        .on('click', this.toggleBigImage.bind(this));
+      $(ACTION_BIG_IMAGE_TOGGLE)
+        .on('click', this.toggleBigImage.bind(this));
     },
 
     actionAssetDownloadShow: function(show) {
@@ -32,9 +34,16 @@
       }
     },
 
-    // openFullscreen: function() {
-    //   $(CONTENT_FULLSCREEN_WRAP).fullscreen(true);
-    // }
+    toggleBigImage: function() {
+      var $use = $(ACTION_BIG_IMAGE_TOGGLE).find('use');
+      $(CONTENT_BIG_IMG_WRAP).closest('.container-fluid')
+        .toggleClass(CONTENT_BIG_IMG_CLASS);
+      if ($use.attr('xlink:href') === '#icon-zoom-in') {
+        $use.attr('xlink:href', '#icon-zoom-out');
+      } else {
+        $use.attr('xlink:href', '#icon-zoom-in');
+      }
+    }
   };
 
   window.AssetPage = AssetPage;
