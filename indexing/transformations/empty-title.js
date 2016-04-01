4a44febb2319855def9142904bb731de93c6bb81
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-04-01 23:19:42 +0200

    The short_title might be null from Cumulus.

--- indexing/transformations/empty-title.js
+++ indexing/transformations/empty-title.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function(state, metadata) {
-  metadata.short_title = metadata.short_title.trim();
+  metadata.short_title = (metadata.short_title || '').trim();
   if (metadata.short_title === '') {
     metadata.short_title = null;
   }
