02d400d72c272e1a1d72dc041bddabc8da04df46
Andreas Larsen <andreas@larsenwork.com>
2016-04-09 10:18:32 +0200

    remove redundant zoom code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,21 +9,6 @@ $(function() {
   var AssetPage = window.AssetPage;
   AssetPage.init();
 
-  // We only want zooming on asset's primary images.
-  $('.asset .zoomable').click(function() {
-    var $thisRow = $(this).closest('.image-row');
-    if ($thisRow.hasClass('col-md-6')) {
-      $thisRow.removeClass('col-md-6').addClass('col-md-12');
-      $thisRow.next('div').addClass('col-md-offset-3')
-        .removeClass('pull-right');
-    } else {
-      $thisRow.removeClass('col-md-12').addClass('col-md-6');
-      $thisRow.next('div').removeClass('col-md-offset-3')
-        .addClass('pull-right');
-    }
-  });
-
-
   var hideAllDropdowns = function(e) {
     var $target = $(e.target);
     var $dropdown = $target.closest('.dropdown');
