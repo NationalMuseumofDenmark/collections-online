d0486603c60d90428c1980c4a877828bf7489e53
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-09-12 13:28:03 +0200

    Elastic searching client side

--- app/scripts-browserify/search.js
+++ app/scripts-browserify/search.js
@@ -1,11 +1,27 @@
 /* global config */
 if(config.features.clientSideSearchResultRendering) {
-  var templates = {
-    searchFilterSidebar: require('views/includes/search-filter-sidebar')
-  };
+  var elasticsearch = require('elasticsearch');
+  var es = new elasticsearch.Client({
+    host: location.origin + '/es',
+    log: config.es.log
+  });
 
-  var hest = templates.searchFilterSidebar();
-  console.log('Hest:', hest);
+  es.search({
+    q: 'hestevogne',
+    index: config.es.assetsIndex
+  }).then(function (body) {
+    console.log('Hits!', body.hits.hits);
+  }, function (error) {
+    console.trace(error.message);
+  });
 
-  
+  if(config.features.searchFilterSidebar) {
+    var templates = {
+      searchFilterSidebar: require('views/includes/search-filter-sidebar')
+    };
+
+    var hest = templates.searchFilterSidebar();
+    console.log('Hest:', hest);
+
+  }
 }
