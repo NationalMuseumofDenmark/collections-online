7b7e8dadff4da5779126a76763e21b67d46ebdb9
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-09-12 15:17:07 +0200

    Rendering the search result assets template

--- app/scripts-browserify/search.js
+++ app/scripts-browserify/search.js
@@ -1,27 +1,38 @@
 /* global config */
 if(config.features.clientSideSearchResultRendering) {
+
+  var templates = {
+    searchFilterSidebar: require('views/includes/search-filter-sidebar'),
+    searchResultAsset: require('views/includes/search-result-asset')
+  };
   var elasticsearch = require('elasticsearch');
   var es = new elasticsearch.Client({
     host: location.origin + '/es',
     log: config.es.log
   });
 
+  var $results = $('#results');
+
   es.search({
     q: 'hestevogne',
     index: config.es.assetsIndex
   }).then(function (body) {
-    console.log('Hits!', body.hits.hits);
+    body.hits.hits.forEach(function(asset) {
+      var markup = templates.searchResultAsset({
+        asset: asset._source
+      });
+      $results.append(markup);
+    });
   }, function (error) {
     console.trace(error.message);
   });
 
+  /*
   if(config.features.searchFilterSidebar) {
-    var templates = {
-      searchFilterSidebar: require('views/includes/search-filter-sidebar')
-    };
 
     var hest = templates.searchFilterSidebar();
     console.log('Hest:', hest);
 
   }
+  */
 }
