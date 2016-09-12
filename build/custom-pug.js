e65f26f77b16b465567b50a999d53ee14b4b8160
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-09-12 16:08:15 +0200

    Loading pug differently

--- build/custom-pug.js
+++ build/custom-pug.js
@@ -1,16 +1,9 @@
-var _ = require('lodash');
-var pug = require('pug');
-
 module.exports = function(config) {
+  var pug = require('../lib/pug')(config);
   var postfix = '\nmodule.exports = template;';
-
-  var pugResolvePlugin = require('../lib/pug-resolve-plugin')(config);
 
   var originalCompileClient = pug.compileClient;
   pug.compileClient = function(contents, opts) {
-    opts = _.merge(opts, {
-      plugins: [pugResolvePlugin]
-    });
     return originalCompileClient(contents, opts) + postfix;
   };
 
