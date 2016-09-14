42151b80e2daa3b0c225bdae4ff8b6edfa1fd13b
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-09-14 15:36:40 +0200

    Moved the dropdown component js to the browserified scripts

---
+++ app/scripts-browserify/dropdown.js
@@ -0,0 +1,55 @@
+console.log('dropdown.js loaded');
+
+var hideAllDropdowns = function(e) {
+  var $target = $(e.target);
+  var $dropdown = $target.closest('.dropdown');
+  var isInputInDropdown = $target.is('input') || $dropdown.size() > 0;
+  if (isInputInDropdown === false) {
+    $('.dropdown').removeClass('dropdown--active');
+    $('body').off('click', hideAllDropdowns);
+  }
+};
+
+$('body').on('click', '.dropdown__selected', function() {
+  var $this = $(this);
+  var $dropdown = $this.closest('.dropdown');
+  var wasActive = $dropdown.hasClass('dropdown--active');
+
+  $('.dropdown').removeClass('dropdown--active');
+
+  if (wasActive === true) {
+    return;
+  }
+
+  $('body').off('click', hideAllDropdowns);
+
+  $dropdown.addClass('dropdown--active');
+
+  setTimeout(function() {
+    $('body').on('click', hideAllDropdowns);
+  }, 1);
+
+});
+
+// Check that dropdown options are on screen
+var repositionDropdowns = function() {
+  $('.dropdown__options').each(function() {
+    var dropOffLeft = $(this).offset().left;
+    var dropdownWidth = $(this).width();
+    var wrapWidth = $('.main-wrapper').width();
+    var isEntirelyVisible = (dropOffLeft + dropdownWidth <=
+      wrapWidth);
+    if (!isEntirelyVisible) {
+      var topOffset = $(this).offset().top;
+      var paddingLeft = $(this).position().left;
+      var newLeftOffset = wrapWidth - dropdownWidth + paddingLeft /
+        2;
+      $(this).offset({
+        top: topOffset,
+        left: newLeftOffset
+      });
+    }
+  });
+};
+
+repositionDropdowns();
--- app/scripts-browserify/index.js
+++ app/scripts-browserify/index.js
@@ -1,1 +1,2 @@
 require('search');
+require('dropdown');
