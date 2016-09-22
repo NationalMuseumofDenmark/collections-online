3bc35daf8750158bb326b5b9f961a50306b4f84d
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-09-22 19:54:12 +0200

    Removed collections-online-cumulus from being a directy dependency to a peer

--- server.js
+++ server.js
@@ -15,9 +15,6 @@ exports.initialize = (app) => {
   process.env.NODE_ENV = process.env.NODE_ENV || 'development';
   var config = require('./lib/config');
 
-  // TODO: Remove collections-online-cumulus as a required dependency
-  require('collections-online-cumulus');
-
   function startServer() {
     console.log('Starting up the server');
     // Start server
