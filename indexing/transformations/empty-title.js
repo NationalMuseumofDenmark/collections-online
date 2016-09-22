03eb37c9c4c533ef20f35af64d2b266af3982c53
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-09-22 18:17:31 +0200

    Moved all indexing to the -cumulus module

--- indexing/transformations/empty-title.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(state, metadata) {
-  metadata.short_title = (metadata.short_title || '').trim();
-  if (metadata.short_title === '') {
-    metadata.short_title = null;
-  }
-  return metadata;
-};
