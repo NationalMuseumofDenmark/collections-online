6688ad05bcc66742289e03721376256358a0aee3
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2016-12-18 14:48:27 +0100

    Moved the thousands seperator helper

--- shared/helpers.js
+++ shared/helpers.js
@@ -1,5 +1,14 @@
 let helpers = {};
 
+// TODO: Consider if a localization function might be easier to use
+helpers.thousandsSeparator = (number) => {
+  if(number) {
+    return number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1.');
+  } else {
+    return number;
+  }
+};
+
 helpers.capitalizeFirstLetter = string => {
   if(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
