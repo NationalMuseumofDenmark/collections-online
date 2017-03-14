b682dc50e8eebbec7054f5d438c00925154c10f5
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-03-14 16:20:10 +0100

    Styling and translation for the auth0 dialog

--- app/scripts-browserify/auth.js
+++ app/scripts-browserify/auth.js
@@ -3,6 +3,15 @@ $(function() {
 
   if(credentials){
     var lock = new Auth0Lock(credentials.clientId, credentials.domain, {
+      languageDictionary: {
+        title: "kbhbilleder.dk"
+      },
+      theme: {
+        logo: '/images/favicons/favicon-96x96.png',
+        labeledSubmitButton: false,
+        primaryColor: 'hsl(340, 88%, 47%)'
+      },
+      language: 'da',
       auth: {
         redirectUrl: credentials.callbackUrl,
         responseType: 'code',
