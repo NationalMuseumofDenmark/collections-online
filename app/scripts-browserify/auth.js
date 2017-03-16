b3e1dc68f99660a82243c0c7dc8745d5a0f37e50
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-16 15:16:08 +0100

    Loading auth0 non-secrets from the config on browserify-time

--- app/scripts-browserify/auth.js
+++ app/scripts-browserify/auth.js
@@ -5,32 +5,26 @@ const config = require('collections-online/shared/config');
 /* global Auth0Lock */
 
 $(function() {
-  var credentials = $('meta[name="auth-credentials"]').attr('content');
-
-  if(credentials){
-    credentials = JSON.parse(credentials);
-
-    var lock = new Auth0Lock(credentials.clientId, credentials.domain, {
-      languageDictionary: {
-        title: config.siteTitle
-      },
-      theme: {
-        logo: '/images/favicons/favicon-96x96.png',
-        labeledSubmitButton: false,
-        primaryColor: config.themeColor
-      },
-      language: 'da',
-      auth: {
-        redirectUrl: credentials.callbackUrl,
-        responseType: 'code',
-        params: {
-          scope: 'openid name email picture'
-        }
+  var lock = new Auth0Lock(config.auth0.clientID, config.auth0.domain, {
+    languageDictionary: {
+      title: config.siteTitle
+    },
+    theme: {
+      logo: '/images/favicons/favicon-96x96.png',
+      labeledSubmitButton: false,
+      primaryColor: config.themeColor
+    },
+    language: 'da',
+    auth: {
+      redirectUrl: config.auth0.callbackURL,
+      responseType: 'code',
+      params: {
+        scope: 'openid name email picture'
       }
-    });
+    }
+  });
 
-    $('[data-action="login"]').on('click',function(){
-      lock.show();
-    });
-  }
+  $('[data-action="login"]').on('click',function(){
+    lock.show();
+  });
 });
