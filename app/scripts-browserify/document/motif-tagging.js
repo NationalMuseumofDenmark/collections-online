e9f32ee646ead416e9f0c9020b0db95602c940ff
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-19 23:35:33 +0100

    Fixed motif tagging

--- app/scripts-browserify/document/motif-tagging.js
+++ app/scripts-browserify/document/motif-tagging.js
@@ -1,217 +1,210 @@
 'use strict';
 
-/* global ga */
-const Snackbar = require('snackbar');
-const contributionCounter = require('./contribution-counter');
+const helpers = require('../../../shared/helpers');
 
-var GA_EVENT_CATEGORY = 'CrowdTag';
-var EDIT_VISION_TAGS_SELECTOR = '.edit-vision-tags';
-var CANCEL_VISION_TAGS_SELECTOR = '.cancel-vision-tags';
-var VISION_CONTAINER_SELECTOR = '.tags-container.vision';
-var VISION_TAGS_SELECTOR = '.tags-container.vision [data-tag]';
-var ADD_VISION_TAG_SELECTOR = '.tags-container.vision .add-tag';
-var $editVisionTags = $(EDIT_VISION_TAGS_SELECTOR);
-var $cancelVisionTags = $(CANCEL_VISION_TAGS_SELECTOR);
-var isEditingVisionTags = false;
-var $visionNoTags = $('.vision .no-tags');
-var $visionBtn = $('#vision-btn');
-var $visionTags = $('.tags-container.vision');
-var $crowdNoTags = $('.crowd .no-tags');
-var $crowdBtn = $('#crowd-btn');
-var $stopCrowdBtn = $('#stop-crowd-btn');
-var $crowdTags = $('.tags-container.crowd');
-var $crowdInput = $('.tags-container.crowd input');
-var $asset = $('.asset');
-var assetId = $asset.data('id');
-var showError = function(msg) {
-  var $error = $('<div class="alert alert-danger">');
-  $error.text(msg);
-  $crowdTags.append($error);
-  ga('send', 'event', GA_EVENT_CATEGORY, 'Error', msg);
-};
-var typeaheadTags = new Bloodhound({
-  datumTokenizer: function(tags) {
-    return Bloodhound.tokenizers.whitespace(tags);
-  },
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  remote: {
-    url: '/motif-tag-suggestions?text=%QUERY',
-    wildcard: '%QUERY',
-    cache: false
-  }
-});
+// Precondition on the availability of the helpers
 
-// FUNCTIONS
-function hasCrowdTags() {
-  return $crowdTags.children('[data-tag]').size() > 0;
+if(!helpers.motifTagging ||
+   typeof(helpers.motifTagging.addTag) !== 'function' ||
+   typeof(helpers.motifTagging.removeTag) !== 'function' ||
+   typeof(helpers.motifTagging.removeVisionTag) !== 'function') {
+  throw new Error('Missing helpers.motifTagging.* helpers');
 }
 
-function hasVisionTags() {
-  return $(VISION_TAGS_SELECTOR).size() > 0;
-}
+const ADD_MOTIF_TAG_SELECTOR = '[data-action="add-motif-tag"]';
+const CANCEL_MOTIF_TAGS_SELECTOR = '[data-action="cancel-motif-tags"]';
+const EDIT_MOTIF_TAGS_SELECTOR = '[data-action="edit-motif-tags"]';
+const SAVE_MOTIF_TAGS_SELECTOR = '[data-action="save-motif-tags"]';
+const TRANSFER_TAG_INPUT_SELECTOR = '[data-action="transfer-motif-tag"]';
+const REMOVE_TAG_INPUT_SELECTOR = '[data-action="remove-motif-tag"]';
 
-function createTagObject(tag, type) {
-  var url = '/?q=' + encodeURIComponent(tag);
-  var tagClassName = 'btn btn-default btn-small';
-  var $new = $('<a href="' + url + '" class="' + tagClassName +
-    '" data-tag="' + tag + '">' + tag + '</a>');
-  if (type === 'vision') {
-    $new.append('<span class="add-tag">' +
-      '<svg><use xlink:href="#icon-thumbup"></use></svg>' +
-      '</span>');
-  }
+const ADD_TAG_INPUT_SELECTOR = '.motif-tagging__add-input input';
 
-  return $new;
-}
+const motifTaggingTemplate = require('views/includes/motif-tagging');
 
-function saveTag(tag) {
-  var url = location.pathname + '/save-crowd-tag';
-  var data = {
-    tag: tag
-  };
-  return $.post(url, data, null, 'json');
-}
+(function($) {
 
-function addTag(input) {
-  input = input || $crowdInput.val();
-  // Don't submit nothing
-  if (input.length !== 0) {
-    var tag = input.trim().toLowerCase();
-    var $new = createTagObject(tag).addClass('saving');
-    // Figure out where to add tag by checking if we already have some tags
-    if (hasCrowdTags() === true) {
-      $('.crowd [data-tag]').last().after($new);
-    } else {
-      $crowdTags.prepend($new);
+  class MotifTaggingController {
+
+    constructor($motifTagging) {
+      this.$motifTagging = $motifTagging;
+      this.state = {
+        editing: false,
+        saving: false,
+        metadata: {}
+      };
+      // Register the listeners
+      this.registerListeners();
     }
-    $crowdInput.typeahead('val', '');
-    $(VISION_CONTAINER_SELECTOR).children('[data-tag="' + tag + '"]')
-      .remove();
 
-    $crowdNoTags.hide();
+    render() {
+      const markup = motifTaggingTemplate(this.state);
+      this.$motifTagging.html(markup);
 
-    // Save tag in cumulus
-    saveTag(tag)
-      .done(function(result) {
-        $new.removeClass('saving');
-        if(result.status === 'success') {
-          Snackbar.info('Gemt! Tak for dit bidrag!');
-          contributionCounter.contributionAdded();
-        } else if(result.status === 'timeout') {
-          Snackbar.info('Gemt! Men endnu ikke opdateret her');
-          contributionCounter.contributionAdded();
-        } else {
-          Snackbar.error('Der skete en fejl');
-        }
-        ga('send', 'event', GA_EVENT_CATEGORY, 'Save', 'Succes');
-      })
-      .fail(function(response) {
-        $new.remove();
-        $crowdInput.typeahead('val', input);
-        showError(response.responseJSON.message || 'Der skete en fejl.');
-        if (hasCrowdTags() === false) {
-          $crowdNoTags.show();
+      const $input = this.$motifTagging.find(ADD_TAG_INPUT_SELECTOR);
+      this.bindTypeahead($input);
+    }
+
+    edit() {
+      // Fetch the current metadata when editing
+      $.getJSON(location.pathname + '/json', (metadata) => {
+        // Fetch and override the metadata
+        this.state.metadata = metadata;
+        // Mutate the state
+        this.state.editing = true;
+        // Re-render the motif-tagging template
+        this.render();
+      });
+    }
+
+    addTagFromInput() {
+      const $input = this.$motifTagging.find(ADD_TAG_INPUT_SELECTOR);
+      // Read the value of the input field
+      const tag = $input.val();
+      // Then clear it
+      $input.val('');
+      // Add it
+      this.addTag(tag);
+    }
+
+    addTag(tag) {
+      // Don't do anything with an undefined, null or empty tag
+      if(!tag || tag === '') {
+        return;
+      }
+      // Add it to the metadata
+      helpers.motifTagging.addTag(this.state.metadata, tag);
+      // Re-render the motif-tagging template
+      this.render();
+    }
+
+    removeTag(tag) {
+      // Remove it from the metadata
+      helpers.motifTagging.removeTag(this.state.metadata, tag);
+      helpers.motifTagging.removeVisionTag(this.state.metadata, tag);
+      // Re-render the motif-tagging template
+      this.render();
+    }
+
+    cancel() {
+      // Fetch the current metadata when editing
+      $.getJSON(location.pathname + '/json', (metadata) => {
+        // Fetch and override the metadata
+        this.state.metadata = metadata;
+        // Mutate the state
+        this.state.editing = false;
+        // Re-render the motif-tagging template
+        this.render();
+      });
+    }
+
+    save() {
+      // Don't save twice
+      if(this.state.saving) {
+        return;
+      }
+      // Add any tag which has not been added
+      this.addTagFromInput();
+      this.state.saving = true;
+      this.render();
+      const url = location.pathname + '/save-motif-tags';
+      $.post(url, this.state.metadata, (metadata) => {
+        // Once the metadata is back, override it and stop saving
+        this.state.metadata = metadata;
+        this.state.saving = false;
+        this.state.editing = false;
+        this.render();
+      }, 'json');
+    }
+
+    registerListeners() {
+      // When clicking the edit button
+      this.$motifTagging.on('click', EDIT_MOTIF_TAGS_SELECTOR, () => {
+        this.edit();
+      });
+      // When clicking the edit button, motif-tagging template is re-rendered
+      this.$motifTagging.on('click', ADD_MOTIF_TAG_SELECTOR, () => {
+        this.addTagFromInput();
+      });
+      // When clicking the edit button, motif-tagging template is re-rendered
+      this.$motifTagging.on('click', CANCEL_MOTIF_TAGS_SELECTOR, () => {
+        this.cancel();
+      });
+      // When clicking the edit button, motif-tagging template is re-rendered
+      this.$motifTagging.on('click', SAVE_MOTIF_TAGS_SELECTOR, () => {
+        this.save();
+      });
+      // When hitting enter in the input field, we add the tag
+      this.$motifTagging.on('keyup', ADD_TAG_INPUT_SELECTOR, (event) => {
+        if (event.keyCode === 13) {
+          this.addTagFromInput();
+          // Put the focus back into the input
+          this.$motifTagging.find(ADD_TAG_INPUT_SELECTOR).focus();
         }
       });
-  } else {
-    console.log('Empty input');
-  }
-}
-
-function confirmVisionTags() {
-  if (hasVisionTags()) {
-    $(VISION_TAGS_SELECTOR).addClass('confirming').on('click', function(e) {
-      e.preventDefault();
-      var $tag = $(this).closest('[data-tag]');
-      var tagName = $tag.attr('data-tag');
-
-      addTag(tagName);
-      ga('send', 'event', GA_EVENT_CATEGORY, 'Add', 'Vision topic');
-      return false;
-    });
-  }
-}
-
-// ACTIONS
-$visionBtn.click(function() {
-  $(this).addClass('loading disabled');
-  $visionNoTags.remove();
-  $.ajax({
-    dataType: 'json',
-    url: location.pathname + '/suggested-motif-tags'
-  }).done(function(data) {
-
-    $visionBtn.remove();
-    console.log(data.tags);
-    var arrayLength = data.tags.length;
-    if (arrayLength !== 0) {
-      for (var i = 0; i < arrayLength; i++) {
-        var tag = data.tags[i];
-        var $tag = createTagObject(tag, 'vision');
-        $visionTags.prepend($tag);
-      }
-
-      $editVisionTags.removeClass('hidden');
+      // When clicking the thumbs up on a vision tag, add it
+      this.$motifTagging.on('click', TRANSFER_TAG_INPUT_SELECTOR, (event) => {
+        const tag = $(event.target).closest('[data-tag]').data('tag');
+        this.addTag(tag);
+        // Let's not make this a click on the actual tag
+        event.preventDefault();
+      });
+      // When clicking the cross on a vision tag, remove it
+      this.$motifTagging.on('click', REMOVE_TAG_INPUT_SELECTOR, (event) => {
+        const tag = $(event.target).closest('[data-tag]').data('tag');
+        this.removeTag(tag);
+        // Let's not make this a click on the actual tag
+        event.preventDefault();
+      });
     }
-  }).fail(function() {
-    console.log('Ajax failed to fetch data');
+
+    bindTypeahead($input) {
+      // Construct a source for tags
+      const tagsSource = new Bloodhound({
+        datumTokenizer: function(tags) {
+          return Bloodhound.tokenizers.whitespace(tags);
+        },
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+          url: '/motif-tag-suggestions?text=%QUERY',
+          wildcard: '%QUERY',
+          cache: false
+        }
+      });
+
+      // Initialize the source of tags
+      tagsSource.initialize();
+
+      // And bind it on the input field
+      $input.typeahead({
+        hint: false,
+        highlight: true,
+        minLength: 1
+      }, {
+        name: 'tags',
+        source: tagsSource
+      });
+
+      // When the input is focussed, add a class to the parent
+      $input.on('focus', function() {
+        $input.parent('.twitter-typeahead').addClass('focussed');
+      });
+
+      // When the input is empty and blurred, remove the class again
+      $input.on('blur', function() {
+        if($input.val() === '') {
+          $input.parent('.twitter-typeahead').removeClass('focussed');
+        }
+      });
+    }
+  }
+
+  // Initialize motif tagging for every .motif-tagging element on the page
+  $(() => {
+    $('.motif-tagging').each((index, motifTagging) => {
+      const $motifTagging = $(motifTagging);
+      // Initialize a controller
+      const controller = new MotifTaggingController($motifTagging);
+    });
   });
-});
-
-$crowdBtn.click(function() {
-  if ($crowdTags.hasClass('inputting')) {
-    addTag();
-    ga('send', 'event', GA_EVENT_CATEGORY, 'Add', 'Button click');
-  } else {
-    confirmVisionTags();
-    $crowdTags.addClass('inputting');
-    $crowdTags.find('input').focus();
-    $stopCrowdBtn.removeClass('hidden');
-    $crowdNoTags.hide();
-  }
-});
-
-$stopCrowdBtn.click(function() {
-  $('.confirming').removeClass('confirming').unbind('click');
-  $crowdInput.parent('span').removeClass('focused valid');
-  $crowdTags.removeClass('inputting');
-  $stopCrowdBtn.addClass('hidden');
-  if (hasCrowdTags() === false) {
-    $crowdNoTags.show();
-  }
-});
-
-$crowdInput.keyup(function(event) {
-  $('.crowd .alert').remove();
-  if (event.keyCode === 13) {
-    addTag();
-    ga('send', 'event', GA_EVENT_CATEGORY, 'Add', 'Enter press');
-    console.log(GA_EVENT_CATEGORY);
-  }
-});
-
-// Classes for input styling purposes
-$crowdInput.focus(function() {
-  $(this).parent('span').addClass('focused valid');
-});
-
-$crowdInput.blur(function() {
-  var $this = $(this);
-  $this.parent('span').removeClass('focused');
-  if ($this.val().length === 0) {
-    $this.parent('span').removeClass('valid');
-  }
-});
-
-
-// Typeahead: Initialize the bloodhound suggestion engine
-typeaheadTags.initialize();
-
-$crowdInput.typeahead({
-  hint: false,
-  highlight: true,
-  minLength: 1
-}, {
-  name: 'tags',
-  source: typeaheadTags
-});
+})(jQuery);
