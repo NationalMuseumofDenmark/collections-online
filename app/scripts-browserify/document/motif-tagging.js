3f05b4c9c30c00286800b2bb14c1ecdae463b585
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-03-27 04:14:12 +0200

    We're using that

--- app/scripts-browserify/document/motif-tagging.js
+++ app/scripts-browserify/document/motif-tagging.js
@@ -6,7 +6,6 @@ const contributionCounter = require('./contribution-counter');
 
 // TODO: Re-introduce the use of Google Analytics events
 // TODO: Add pretty errors instead of silent fails
-// TODO: Use the "require('./contribution-counter')"
 
 // Precondition on the availability of the helpers
 
