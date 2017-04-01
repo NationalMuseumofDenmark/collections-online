caf3b6ecb22de5462d6a63d45adf8605d44cbe12
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-04-01 23:56:46 +0200

    Enabled swiping and the arrows now disappear after 3 secs

--- app/scripts-browserify/document/navigator.js
+++ app/scripts-browserify/document/navigator.js
@@ -1,6 +1,8 @@
+const Hammer = require('hammerjs');
 const helpers = require('../../../shared/helpers');
 
 const PAGE_SIZE = 20;
+const HIDING_TIMEOUT = 3000;
 
 const $left = $('a.document__navigator-arrow--left');
 const $right = $('a.document__navigator-arrow--right');
@@ -12,6 +14,18 @@ const elasticsearch = require('elasticsearch');
 const es = new elasticsearch.Client({
   host: location.origin + '/api'
 });
+
+const ARROWS = {
+  'right': $right,
+  'left': $left
+};
+const DIRECTIONS = {
+  2: 'right',
+  4: 'left'
+};
+
+const navigationQueue = [];
+const urls = {};
 
 const navigator = {
   save: (state) => {
@@ -26,17 +40,61 @@ const navigator = {
     const searchString = window.sessionStorage.getItem('search');
     return searchString ? JSON.parse(searchString) : {};
   },
-  initializeArrow($arrow, hit) {
+  initializeArrow: (direction, hit) => {
+    const $arrow = ARROWS[direction];
     const url = helpers.getDocumentURL(hit.metadata);
+    // Save this url such that the swipe listener can change location
+    urls[direction] = url;
     $arrow.attr('href', url);
-    $arrow.removeClass(HIDDEN_CLASS);
     const $preview = $arrow.find('.document__navigator-preview');
     $preview.html(navigatorPreview({
       helpers,
       metadata: hit.metadata
     }));
+    // Showing the arrow
+    navigator.showArrow($arrow);
+    $(window).on('mousemove', () => {
+      navigator.showArrow($arrow);
+    }).on('touchstart', () => {
+      navigator.showArrow($arrow);
+    }).on('touchmove', () => {
+      navigator.showArrow($arrow);
+    });
+  },
+  navigate: direction => {
+    if(direction in urls) {
+      location.href = urls[direction];
+    } else {
+      navigationQueue.push(direction);
+    }
+  },
+  startHidingArrow: $arrow => {
+    let hideTimeout = $arrow.data('hide-timeout');
+    if(hideTimeout) {
+      clearTimeout(hideTimeout);
+    }
+    // The arrows hide when the screen is not interacted with
+    hideTimeout = setTimeout(() => {
+      // Time is up
+      $arrow.addClass(HIDDEN_CLASS);
+    }, HIDING_TIMEOUT);
+    $arrow.data('hide-timeout', hideTimeout);
+  },
+  showArrow: $arrow => {
+    $arrow.removeClass(HIDDEN_CLASS);
+    navigator.startHidingArrow($arrow);
   }
 };
+
+// Register a listener for the swipe gesture
+$('.document').each((i, doc) => {
+  const hammer = new Hammer(doc);
+  hammer.on('swipe', (e) => {
+    if(e.direction in DIRECTIONS) {
+      navigator.navigate(DIRECTIONS[e.direction]);
+    }
+  });
+});
 
 // Check if the session storage is available
 if(window.sessionStorage) {
@@ -57,12 +115,12 @@ if(window.sessionStorage) {
     if(currentIndex > -1) {
       if(previousIndex >= 0) {
         const previous = resultsLoaded[previousIndex];
-        navigator.initializeArrow($left, previous);
+        navigator.initializeArrow('left', previous);
       }
 
       if(nextIndex < resultsLoaded.length) {
         const next = resultsLoaded[nextIndex];
-        navigator.initializeArrow($right, next);
+        navigator.initializeArrow('right', next);
       } else {
         // Fetch some more results
         resultsDesired += PAGE_SIZE;
@@ -85,7 +143,7 @@ if(window.sessionStorage) {
             });
             // Now we can initialize
             const next = resultsLoaded[nextIndex];
-            navigator.initializeArrow($right, next);
+            navigator.initializeArrow('right', next);
           }
         });
       }
