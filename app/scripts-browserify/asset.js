c6ace0fbd94cd7102ea533353c3fbd171fcdeaa5
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-04-26 15:01:24 +0200

    Cleaning up asset.js

--- app/scripts-browserify/asset.js
+++ app/scripts-browserify/asset.js
@@ -3,14 +3,8 @@
 const config = require('collections-online/shared/config');
 
 (function($, window) {
-  var ACTION_ASSET_DOWNLOAD_SHOW = '[data-action="asset-download-show"]';
-  var ACTION_ASSET_ZOOMABLE = '.asset-image--zoomable';
-  var ACTION_BIG_IMAGE_TOGGLE = '[data-action="asset-image-size-toggle"]:not(.dimmed)';
-  var ACTION_BIG_IMAGE_DISABLED = '[data-action="asset-image-size-toggle"].dimmed';
-  var CONTENT_ASSET_TOP = '.document__top';
+  var ACTION_ASSET_DOWNLOAD_SHOW = '[data-action="download:show-overlay"]';
   var CONTENT_ASSET_DOWNLOAD = '[data-content="asset-download"]';
-  var CONTENT_ASSET_NO_ZOOM = '.no-zoom-message';
-  var CONTENT_EXPANDED_CLASS = 'expanded';
   var CONTENT_SLIDER = '.slider';
   var OVERLAY_ACTIVE_CLASS = 'overlay__container--active';
   var OVERLAY_ANIM_IN_CLASS = 'overlay__container--anim-in';
@@ -21,14 +15,6 @@ const config = require('collections-online/shared/config');
         .on('click', this.actionAssetDownloadShow.bind(this, true));
       $(CONTENT_ASSET_DOWNLOAD)
         .on('click', this.actionAssetDownloadShow.bind(this, false));
-      $(ACTION_BIG_IMAGE_DISABLED)
-        .on('click', this.actionAssetNoZoom.bind(this, true));
-      $(CONTENT_ASSET_NO_ZOOM)
-        .on('click', this.actionAssetNoZoom.bind(this, false));
-      $(ACTION_ASSET_ZOOMABLE)
-        .on('click', this.toggleBigImage.bind(this));
-      $(ACTION_BIG_IMAGE_TOGGLE)
-        .on('click', this.toggleBigImage.bind(this));
 
       if ($(CONTENT_SLIDER).size() > 0) {
         $(CONTENT_SLIDER).slick({
@@ -74,38 +60,6 @@ const config = require('collections-online/shared/config');
         setTimeout(function() {
           $el.removeClass(OVERLAY_ACTIVE_CLASS);
         }, 300);
-      }
-    },
-
-    actionAssetNoZoom: function(show) {
-      var $el = $(CONTENT_ASSET_NO_ZOOM);
-      if (show === true) {
-        $el.addClass(OVERLAY_ACTIVE_CLASS);
-        $el.addClass(OVERLAY_ANIM_IN_CLASS);
-      } else if (show === false) {
-        $el.removeClass(OVERLAY_ANIM_IN_CLASS);
-        setTimeout(function() {
-          $el.removeClass(OVERLAY_ACTIVE_CLASS);
-        }, 300);
-      }
-    },
-
-    toggleBigImage: function() {
-      $(CONTENT_ASSET_TOP).toggleClass(CONTENT_EXPANDED_CLASS);
-      if(config.features.lazyLoadExpandedAssets) {
-        // Make sure that an image that needs lazyloading gets its src replaced
-        var expanded = $(CONTENT_ASSET_TOP).hasClass(CONTENT_EXPANDED_CLASS);
-        $('img[data-expanded-src]', CONTENT_ASSET_TOP).each(function() {
-          if (expanded) {
-            // Save the original src for later
-            var originalSrc = $(this).attr('src');
-            $(this).data('original-src', originalSrc);
-            // Replace the src
-            $(this).attr('src', $(this).data('expanded-src'));
-          } else {
-            $(this).attr('src', $(this).data('original-src'));
-          }
-        });
       }
     }
   };
