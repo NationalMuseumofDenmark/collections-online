2c32806635ef79f0d19f40e419874228cc515b63
Viktor Shamal Andersen <sphinxominator@gmail.com>
2017-04-26 14:12:04 +0200

    Added error and success messages to feedback

--- app/scripts-browserify/document/feedback.js
+++ app/scripts-browserify/document/feedback.js
@@ -9,7 +9,10 @@ const FORM_SELECTOR = '.feedback__form';
 const COUNTER_SELECTOR = '.feedback__counter';
 const EDITING_CONTAINER_SELECTOR = '.feedback__container';
 
+const BASE_CLASS = 'feedback';
 const IS_EDITING_CLASS = 'feedback--editing';
+const ERROR_CLASS = 'feedback--error';
+const SUCCESS_CLASS = 'feedback--success';
 
 $(function() {
   class FeedbackController {
@@ -23,20 +26,34 @@ $(function() {
       this.registerListeners();
     }
 
+    reset() {
+      this.$feedback.attr('class', BASE_CLASS);
+    }
+
     toggleEditing(editing) {
+      if(editing) {
+        this.$feedback.removeClass(ERROR_CLASS + ' ' + SUCCESS_CLASS)
+      }
       this.$feedback.toggleClass(IS_EDITING_CLASS, editing);
+    }
+
+    handleError() {
+      this.$feedback.addClass(ERROR_CLASS);
+    }
+
+    handleSuccess() {
+      this.$feedback.addClass(SUCCESS_CLASS);
+      this.toggleEditing(false);
     }
 
     send() {
       const url = location.pathname + '/feedback';
       const message = this.$form.val();
       this.$sendButton.attr('disabled', true);
-      $.post(url, {message}, (response) => {
-      }, 'json')
-      .done(()=> {
-        this.toggleEditing(false);
-        this.$sendButton.attr('disabled', false);
-      })
+      $.post(url, {message}, () => {}, 'json')
+      .done(() => this.handleSuccess())
+      .fail(() => this.handleError())
+      .always(() => this.$sendButton.attr('disabled', false))
     }
 
     registerListeners() {
